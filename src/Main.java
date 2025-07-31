f5e90d3017afcb8c1895ea2654a8ef4ac30cf432
Ralph <rjteodora04981@liceo.edu.ph>
2025-07-31 11:47:01 +0800

    activity 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] rags) {
-        System.out.println("Hello World!");
+
+    String StudentID = "";
+    String FirstName = "";
+    String LastName  = "";
+    String Course = "";
+    String Section = "";
+
+    //Student Grade
+    int Midterm = 0;
+    int Final = 0;
+    int Project = 0;
+    int AttendancePercentage = 0;
+
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+
+
+        System.out.print("Student ID: ");
+        String StudentID = Scan.nextLine();
+
+        System.out.print("Last name: ");
+        String LastName = Scan.nextLine();
+
+        System.out.print("First name: ");
+        String FirstName = Scan.nextLine();
+
+        System.out.print("Course: ");
+        String Course = Scan.nextLine();
+
+        System.out.print("Section: ");
+        String Section = Scan.nextLine();
+
+        System.out.println(LastName + " " + FirstName + " " + Course + " " + Section);
+
+
+        System.out.print("Input your scores in Midterm Exam: ");
+        int Midterm = Scan.nextInt();
+
+        System.out.print("Input your scores in Final Exam: ");
+        int Final = Scan.nextInt();
+
+        System.out.print("Input your scores in your project: ");
+        int Project = Scan.nextInt();
+
+        System.out.print("Input your scores in your Attendance: ");
+        int Atten = Scan.nextInt();
+
+        int allOverScore = Midterm + Final + Project + Atten;
+
+        double averageScore = allOverScore / 400.0 * 100;
+
+        if (averageScore > 75) {
+            System.out.println("PASSED! Your average is:" + averageScore);
+        }
+        else {
+            System.out.println("FAILED. Your average is:" + averageScore);
+        }
+
+        Scan.close();
     }
 }
