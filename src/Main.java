4e5ddd1faae5dbec3d7a10a0418a8173738165d5
Ralph <rjteodora04981@liceo.edu.ph>
2025-08-01 09:51:35 +0800

    lab-acitivity-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] rags) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalNumber = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter a number " + i + ": ");
+            int number = scanner.nextInt();
+            totalNumber += number;
+        }
+
+        System.out.println("Total:  " + totalNumber);
+
+        scanner.close();
     }
 }
