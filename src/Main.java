4ab5e5e34e1ac7efd1aa293be31044bb728a4105
Ralph <rjteodora04981@liceo.edu.ph>
2025-08-08 10:20:01 +0800

    lab-activity-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] rags) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        book book1 = new book("The Krokonz", "Ralph Teodora", 80);
+        book book2 = new book("Dooms Day", "SHOTE CAPINPUYAN", 60);
+        book book3 = new book("Bigay Mo Sakin Saber LVL 4 Story", "Rain Bacabis", 100);
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book1.displayInfo();
+
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
