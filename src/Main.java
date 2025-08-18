421da87f022d94a6e1ff85270ca60841dc46a1eb
Ralph <rjteodora04981@liceo.edu.ph>
2025-08-18 11:39:03 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] rags) {
-        System.out.println("Hello World!");
+
+public static void main(String[] args) {
+    GradeCalculator GC = new GradeCalculator();
+
+    String student = "John Smith";
+    double average = GC.calculateAverage(85.5, 92.0, 78.5, 90.0);
+    String grade = GC.getLetterGrade(average);
+
+    GC.displayResult(student, average);
+    GC.displayResult(student, average, grade);
     }
 }
